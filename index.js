87c63bbe2c1309c4a2378d126b2fcd8bf6773672
Sandro Saldutto <sandro.saldutto@hotmail.ca>
2022-03-09 11:57:05 -0500

    both get routes are working

--- index.js
+++ index.js
@@ -7,15 +7,9 @@ app.use(express.json())
 
 app.use(cors());
 
-// app.use((req, res, next) => {
-//     res.status(200).send({
-//         message: "successful"
-//     })
-// })
-
 // routes
 const videosRoutes = require("./routes/videos");
-app.use("/", videosRoutes);
+app.use("/videos", videosRoutes);
 
 // listen
 app.listen(8080, () => {
--- routes/videos.js
+++ routes/videos.js
@@ -2,20 +2,40 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 
+const getVideos = () => {
+  const videos = fs.readFileSync("./data/videos.json");
+  return JSON.parse(videos);
+};
 
-router.route("/").get(( _req, res ) => {
-res.status(200).send({
-    message: "home"
+router.route("/").get((_req, res) => {
+  let videosConfig = getVideos().map((video) => {
+    return {
+      id: video.id,
+      title: video.title,
+      channel: video.channel,
+      image: video.image,
+      description: video.description,
+      views: video.views,
+      likes: video.likes,
+      timestamp: video.timestamp,
+      comments: video.comments,
+    };
+  });
+
+  res.status(200).json(videosConfig);
+});
+
+router.get('/:videoId', (req, res) => {
+    const singleVideo = getVideos().find(video => video.id === req.params.videoId);
+    console.log(getVideos(), singleVideo)
+    if (!singleVideo) {
+        res.status(404).json({
+            message: "Video not found"
+        })
+        return;
+    }
+    
+    res.status(200).json(singleVideo)
 })
-})
-
-router.get("/videos", ( _req, res ) =>{
-    res.status(200).send({
-        message: "videos"
-    })
-})
-
-
-  
 
 module.exports = router;
