8970b7cddafba0bcae963e9ed5c92370e7fc3c80
Sandro Saldutto <sandro.saldutto@hotmail.ca>
2022-03-09 21:33:54 -0500

    implementing the post feature for the new video data but not connecting

--- routes/videos.js
+++ routes/videos.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const req = require("express/lib/request");
 const router = express.Router();
 const fs = require("fs");
+const {v4: uuidv4 } = require("uuid");
 
 
 const getVideos = () => {
@@ -8,7 +10,12 @@ const getVideos = () => {
   return JSON.parse(videos);
 };
 
-router.route("/").get((_req, res) => {
+const storeVideos = (videos) => {
+  fs.writeFileSync('./data/videos.json', JSON.stringify(videos))
+}
+
+router.route("/")
+.get((_req, res) => {
   let videosConfig = getVideos().map((video) => {
     return {
       id: video.id,
@@ -20,10 +27,37 @@ router.route("/").get((_req, res) => {
       likes: video.likes,
       timestamp: video.timestamp,
       comments: video.comments,
+      duration: video.duration,
+      video: video.video,
     };
   });
 
   res.status(200).json(videosConfig);
+})
+.post((_req,res) => {
+  const newVideo = {
+    id: uuidv4(),
+    title: req.body.title,
+    channel: req.body.channel,
+    image: req.body.image,
+    description: req.body.description,
+    views: req.body.views,
+    likes: req.body.likes,
+    timestamp: req.body.timestamp,
+    comments: req.body.comments,
+    duration: req.body.duration,
+    video: req.body.video,
+  }
+
+  let videos = getVideos();
+  videos.push(newVideo)
+
+  storeVideos(videos)
+
+  res.status(201).send({
+    "id": newVideo.id,
+    "status": "new video has been added"
+  })
 });
 
 router.get('/:videoId', (req, res) => {
@@ -39,4 +73,6 @@ router.get('/:videoId', (req, res) => {
     res.status(200).json(singleVideo)
 })
 
+
+// post comments 
 module.exports = router;
